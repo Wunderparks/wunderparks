cdf3d492ca64422d392ff59b100edfe1971d8d2e
Luke Driscoll <lukedriscoll@Lukes-MacBook-Air.local>
2022-12-11 20:03:50 -0500

    add routes for getting park info, getting all parks, creating a user, adding a park

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: String,
-  parksCompleted: [String],
-});
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    parksVisited: {},
+  },
+  { minimize: false }
+);
 
 module.exports = mongoose.model('user', userSchema);
+// parksVisited:
+// yose: {
+//   rating: Number,
+//   date: Date,
+//   notes: String,
+//   activitiesCompleted: Array}
+// acad: {
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -3,10 +3,25 @@ const userController = require('../controllers/UserController');
 
 const userRouter = express.Router();
 
+userRouter.get(
+  '/:parkCode',
+  userController.getUser,
+  userController.getParkInfo,
+  (_req, res) => {
+    return res.status(200).json(res.locals.parkInfo);
+  }
+);
+
 userRouter.get('/', userController.getParks, (_req, res) => {
   return res.status(200).json(res.locals.parks);
 });
 
-// userRouter.use();
+userRouter.post('/', userController.createUser, (_req, res) => {
+  return res.status(200).json(res.locals.newUser);
+});
+
+userRouter.post('/:parkCode', userController.addPark, (_req, res) => {
+  return res.status(200).json(res.locals.park);
+});
 
 module.exports = userRouter;
