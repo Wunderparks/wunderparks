b359c75bc506c04e4fe987b7297070917ffe7ecf
kimballcaroline <kimballcaroline@gmail.com>
2022-12-13 16:04:43 -0500

    Ran npm install and build

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useLayoutEffect } from 'react';
-import SidebarContainer from '../containers/SidebarContainer.jsx';
-import MainContainer from '../containers/MainContainer.jsx';
+import React, { useEffect, useState, useLayoutEffect } from "react";
+import SidebarContainer from "../containers/SidebarContainer.jsx";
+import MainContainer from "../containers/MainContainer.jsx";
 
 const App = () => {
   // let codes = [];
@@ -11,23 +11,22 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetch('http://localhost:3000/user/', {
-      method: 'GET',
-      headers: { 'Content-Type': 'Application/JSON' },
+    fetch("http://localhost:3000/user/", {
+      method: "GET",
+      headers: { "Content-Type": "Application/JSON" },
     })
       .then((res) => res.json())
       .then((data) => {
         setCodes(data);
       })
-      .catch((err) => console.log('AddPark fetch POST to api: ERROR: ', err));
+      .catch((err) => console.log("AddPark fetch POST to api: ERROR: ", err));
   }, []);
 
-
   return (
-    <div className="app">
+    <div className='app'>
       <SidebarContainer codes={codes} />
-      <div className="right">
-        <div className="float">
+      <div className='right'>
+        <div className='float'>
           <h1> WÜNDER PARKS</h1>
         </div>
         <MainContainer codes={codes} />
