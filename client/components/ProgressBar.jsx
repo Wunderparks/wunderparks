724175396353edd4ea5524defcd48297c67d5099
Amrit Ramos <amrit.ramos@gmail.com>
2022-12-13 17:56:22 -0300

    .env created

--- client/components/ProgressBar.jsx
+++ client/components/ProgressBar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 function WithLabelExample(props) {
-  const now = Math.round((props.percent.length)/63*100, 2)
+  const now = Math.round((props.percent.length / 63) * 100, 2);
   return <ProgressBar now={now} label={`${now}%`} />;
 }
 
