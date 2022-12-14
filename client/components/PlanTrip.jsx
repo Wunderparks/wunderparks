122204c4c1746c86984f8149217c10f6ab8cbe99
kimballcaroline <kimballcaroline@gmail.com>
2022-12-13 20:10:32 -0500

    Updated styling on Log Your Trip page

---
+++ client/components/PlanTrip.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+const PlanTrip = (props) => {
+  // pass in name and any user info through props
+  //
+
+  //look through the api to see what filters we can apply
+  //then we need to render the IconsMaker ONLY with the filters we've applied
+
+  //filters:
+  //amenities/parksplaces
+  //activities/parks
+  //thingstodo
+  //topics/parks
+
+  return (
+    <div>
+      <h1>Plan Your Trip</h1>
+      <div className='formWrapper'>
+        <form action=''></form>
+      </div>
+    </div>
+  );
+};
+
+export default PlanTrip;
